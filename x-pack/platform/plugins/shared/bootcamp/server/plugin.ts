4a7b62dff6a77539d9e662b45ed6d944ee3f771c
Miguel Mart'in <miguelmartin@Miguels-MacBook-Pro.local>
2025-10-20 20:43:08 +0100

    test

--- x-pack/platform/plugins/shared/bootcamp/server/plugin.ts
+++ x-pack/platform/plugins/shared/bootcamp/server/plugin.ts
@@ -1,53 +1,74 @@
-import type { PluginInitializerContext, Logger, CoreSetup, CoreStart, Plugin } from "@kbn/core/server";
-import type { BootcampServerPluginSetupDeps, BootcampServerPluginStartDeps, BootcampServerSetup, BootcampServerStart } from "./types";
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
 
-export class BootcampPlugin implements Plugin<
-    BootcampServerSetup,
-    BootcampServerStart,
-    BootcampServerPluginSetupDeps,
-    BootcampServerPluginStartDeps
-> {
+import type {
+  PluginInitializerContext,
+  Logger,
+  CoreSetup,
+  CoreStart,
+  Plugin,
+} from '@kbn/core/server';
+import type {
+  BootcampServerPluginSetupDeps,
+  BootcampServerPluginStartDeps,
+  BootcampServerSetup,
+  BootcampServerStart,
+} from './types';
 
-    private readonly logger: Logger;
+export class BootcampPlugin
+  implements
+    Plugin<
+      BootcampServerSetup,
+      BootcampServerStart,
+      BootcampServerPluginSetupDeps,
+      BootcampServerPluginStartDeps
+    >
+{
+  private readonly logger: Logger;
 
-    constructor(private readonly initContext: PluginInitializerContext) {
-        this.logger = initContext.logger.get();
-    }
+  constructor(private readonly initContext: PluginInitializerContext) {
+    this.logger = initContext.logger.get();
+  }
 
+  setup(
+    core: CoreSetup<BootcampServerPluginStartDeps, BootcampServerStart>,
+    plugins: BootcampServerPluginSetupDeps
+  ): BootcampServerSetup {
+    this.logger.info('Bootcamp plugin setup');
 
-    setup(core: CoreSetup<BootcampServerPluginStartDeps, BootcampServerStart>, plugins: BootcampServerPluginSetupDeps): BootcampServerSetup {
-        this.logger.info('Bootcamp plugin setup');
+    const bootcampLogService = {
+      warn: (message: string) => this.logger.warn(message),
+      error: (message: string) => this.logger.error(message),
+    };
 
-        const bootcampLogService = {
-            warn: (message: string) => this.logger.warn(message),
-            error: (message: string) => this.logger.error(message),
-        }
+    core.getStartServices().then(([coreStart, pluginsStart, bootcampStart]) => {
+      bootcampStart.bootcampLogService.warn('Bootcamp plugin setup');
+      // pluginsStart.fieldsMetadata?.getClient()
+    });
 
-        core.getStartServices()
-            .then(([coreStart, pluginsStart, bootcampStart]) => {
-                bootcampStart.bootcampLogService.warn('Bootcamp plugin setup');
-                // pluginsStart.fieldsMetadata?.getClient()
-            });
+    return {
+      bootcampLogService,
+    };
+  }
 
-        return {
-            bootcampLogService
-        };
-    }
+  start(core: CoreStart, plugins: BootcampServerPluginStartDeps): BootcampServerStart {
+    this.logger.info('Bootcamp plugin start');
 
-    start(core: CoreStart, plugins: BootcampServerPluginStartDeps): BootcampServerStart {
-        this.logger.info('Bootcamp plugin start');
+    // if (plugins.fieldsMetadata) {
+    //     plugins.fieldsMetadata.getClient();
+    // }
 
-        // if (plugins.fieldsMetadata) {
-        //     plugins.fieldsMetadata.getClient();
-        // }
+    const bootcampLogService = {
+      warn: (message: string) => this.logger.warn(message),
+      error: (message: string) => this.logger.error(message),
+    };
 
-        const bootcampLogService = {
-            warn: (message: string) => this.logger.warn(message),
-            error: (message: string) => this.logger.error(message),
-        }
+    return { bootcampLogService };
+  }
 
-        return { bootcampLogService };
-    }
-
-    // stop() {}
+  // stop() {}
 }
--- x-pack/platform/plugins/shared/bootcamp/server/types.ts
+++ x-pack/platform/plugins/shared/bootcamp/server/types.ts
@@ -1,31 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
 
-import { SpacesPluginSetup, SpacesPluginStart } from '@kbn/spaces-plugin/server';
-import { FieldsMetadataServerSetup, FieldsMetadataServerStart } from '@kbn/fields-metadata-plugin/server';
+import type { SpacesPluginSetup, SpacesPluginStart } from '@kbn/spaces-plugin/server';
+import type {
+  FieldsMetadataServerSetup,
+  FieldsMetadataServerStart,
+} from '@kbn/fields-metadata-plugin/server';
 
 export interface BootcampServerPluginSetupDeps {
-    // Add dependencies here
+  // Add dependencies here
 
-    spaces: SpacesPluginSetup;
-    fieldsMetadata?: FieldsMetadataServerSetup
+  spaces: SpacesPluginSetup;
+  fieldsMetadata?: FieldsMetadataServerSetup;
 }
 
 export interface BootcampServerSetup {
-    // Expose services here
-    bootcampLogService: {
-        warn: (message: string) => void;
-        error: (message: string) => void;
-    }
+  // Expose services here!
+  bootcampLogService: {
+    warn: (message: string) => void;
+    error: (message: string) => void;
+  };
 }
 
 export interface BootcampServerPluginStartDeps {
-    // Add dependencies here. Aligned with the BootcampServerPluginSetupDeps
-    spaces: SpacesPluginStart;
-    fieldsMetadata?: FieldsMetadataServerStart;
+  // Add dependencies here. Aligned with the BootcampServerPluginSetupDeps
+  spaces: SpacesPluginStart;
+  fieldsMetadata?: FieldsMetadataServerStart;
 }
 
 export interface BootcampServerStart {
-    bootcampLogService: {
-        warn: (message: string) => void;
-        error: (message: string) => void;
-    }
+  bootcampLogService: {
+    warn: (message: string) => void;
+    error: (message: string) => void;
+  };
 }
